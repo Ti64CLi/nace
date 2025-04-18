e32e1fe5b716c75490bbe6739500edc7a2f91c03
Elie Dumont <elie.dumont@ens-rennes.fr>
2025-04-18 18:41:40 +0200

    Fix inline helpers blocking the compilation

--- src/engine/utils.h
+++ src/engine/utils.h
@@ -1,8 +1,34 @@
 #pragma once
 
+/*
 unsigned char to_lower_case(unsigned char c);
 unsigned char to_upper_case(unsigned char c);
 bool is_lower_case(unsigned char c);
 bool is_upper_case(unsigned char c);
 bool is_alpha(unsigned char c);
 bool is_numerical(unsigned char c);
+*/
+
+inline bool is_lower_case(unsigned char c) {
+    return ((c) >= 'a' && (c) <= 'z');
+}
+
+inline bool is_upper_case(unsigned char c) {
+    return ((c) >= 'A' && (c) <= 'Z');
+}
+
+inline bool is_alpha(unsigned char c) {
+    return (is_lower_case(c) || is_upper_case(c));
+}
+
+inline bool is_numerical(unsigned char c) {
+    return ((c) >= '0' && (c) <= '9');
+}
+
+inline unsigned char to_lower_case(unsigned char c) {
+    return  (is_upper_case(c) ? (c) + ('a' - 'A') : (c));
+}
+
+inline unsigned char to_upper_case(unsigned char c) {
+    return (is_lower_case(c) ? (c) - 'a' + 'A' : (c));
+}
