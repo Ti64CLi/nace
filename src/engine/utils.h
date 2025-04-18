c998243f6100c3db55433e88a548d2a8be386ba8
Elie Dumont <elie.dumont@ens-rennes.fr>
2025-04-18 18:22:19 +0200

    Declare some general purpose functions

---
+++ src/engine/utils.h
@@ -0,0 +1,8 @@
+#pragma once
+
+unsigned char to_lower_case(unsigned char c);
+unsigned char to_upper_case(unsigned char c);
+bool is_lower_case(unsigned char c);
+bool is_upper_case(unsigned char c);
+bool is_alpha(unsigned char c);
+bool is_numerical(unsigned char c);
